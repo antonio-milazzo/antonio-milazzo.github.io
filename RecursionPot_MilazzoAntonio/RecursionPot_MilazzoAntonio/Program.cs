246fe827b86ec58e356941bbf1ea4842cfec8890
Antonio Milazzo <82915648+antonio-milazzo@users.noreply.github.com>
2023-12-06 13:12:15 -0600

    Add files via upload

---
+++ RecursionPot_MilazzoAntonio/RecursionPot_MilazzoAntonio/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace RecursionPot_MilazzoAntonio
+/*
+* //TODO: Antonio Milazzo
+* Recursion Assignment
+* //TODO: 10/4/23
+* Based on Dogon Pot pseudo code from the book
+* African Fractals: Modern Computing and Indigenous Design by Ron Eglash
+*/
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Example steps to draw a Dogon-like pot.";
+            new Pot();
+            Console.ReadKey();
+
+        }
+    }
+}
